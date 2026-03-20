8728587116f6474e13c016adee422f8312f9cad8
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-20 09:45:01 +0100

    bump ApprovalTests to 10.13.0
    in particular this has catch2v3 support which is typically
    what modern distros provide on a system level

--- cmake/Modules/FindApprovalTests.cmake
+++ cmake/Modules/FindApprovalTests.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
         ApprovalTests
         GIT_REPOSITORY https://github.com/approvals/ApprovalTests.cpp.git
-        GIT_TAG v.10.12.2
+        GIT_TAG v.10.13.0
 )
 FetchContent_MakeAvailable(ApprovalTests)
